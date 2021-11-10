643114ad29b7746436c0963db739e4cbc34ded30
Austin Thornley <austinjt1012@gmail.com>
2021-11-10 15:51:32 -0700

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+	<p>Open modal below</p>
+	<div v-if="showModal">
+  	<Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
+	</div>
+	<button @click.alt="toggleModal">Open Modal (alt)</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App!! :)',
+			header: 'Enter the giveaway now!',
+			text: 'Win any vinyl of your choice!',
+			showModal: false
+    };
+  },
+	methods: {
+		toggleModal() {
+			this.showModal = !this.showModal;
+		}
+	}
+};
 </script>
 
 <style>
@@ -22,5 +37,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
